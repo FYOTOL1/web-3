1676a4b8376749e3c400969d2e200b1b079e83b7
EGY-Temble <fyotol@gmail.com>
2023-06-08 06:21:57 +0300

    nothing

---
+++ main.js
@@ -0,0 +1,10 @@
+let img = document.querySelector(".imo");
+let container = document.getElementById("container")
+
+function phones(phone) {
+    img.src = phone;
+};
+
+function colors(color) {
+    container.style.background = color;
+}
